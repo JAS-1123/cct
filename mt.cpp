51cd02df95cc9eaa304f152924846ba322a46a7d
JAS1123 <jaiaditya2234@gmail.com>
2026-01-15 17:34:19 +0530

    Add initial implementation of mt.cpp

---
+++ mt.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef long long ll;typedef vector<ll> vll;
+#define endl '\n'
+#define as(v) sort(v.begin(), v.end())
+#define ds(v) sort(v.begin(), v.end(), greater<>())
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define f first
+#define s second
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b)))
+template <class T> void input(vector<T> &v){for(auto &x:v)cin>>x;}
+void cout_vll(const vll&arr){for(auto it:arr){cout<<it<<" ";}cout<<endl;}
+
+void solve(){
+
+        
+
+}
+
+int main(){ios_base::sync_with_stdio(false);cin.tie(NULL);
+ll SingleTest = 0;
+if(SingleTest){solve();}else{int tt; cin >> tt; while(tt--){solve();}}}
